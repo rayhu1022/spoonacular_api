b907e0000c25f465ad8c3d8ee8291b5eaf43b14f
Ray Hu <yunghu001@gmail.com>
2022-12-14 15:08:45 -0800

    added tests

--- tests/test_spoonacular_api.py
+++ tests/test_spoonacular_api.py
@@ -1,15 +1,26 @@
 from spoonacular_api.spoonacular_api import GroceryList
 import pandas as pd
+from dotenv import load_dotenv
+import os
 
-API_Key = '98d4afc5d8f34efbbea6ccda53a7f965'
+load_dotenv()
+API_Key = os.getenv('API_Key')
 
 test = GroceryList('username', 'firstname', 'lastname', 'user@email.com', API_Key)
 
 def test_searchRecipe():
-    data = {'Recipe': ['Spinach Soup With Wontons', 'The Best Quinoa Minsetrone', 'Pomegranate Soup (Ash-E Anar)'], 
+    data = {'Recipe': ['Spinach Soup With Wontons', 'The Best Quinoa Minestrone', 'Pomegranate Soup (Ash-E Anar)'], 
             'Servings': [4, 8, 6], 
             'Price Per Serving': [2.43, 2.92, 2.05],
             'Recipe ID': [661351, 663227, 656635]}
     df = pd.DataFrame(data)
     df2 = test.searchRecipe('soup', 'chicken', 'spinach', 'pepper')
+    assert df.equals(df2)
+
+def test_getIngredient():
+    data = {'Ingredient': ['mushrooms', 'baby spinach', 'shallot', 'parmesan cheese', 'eggs', 'olive oil', 'butter', 'salt and pepper'], 
+            'Amount': [96, 60, 2.00, 25.00, 3.00, 0.50, 0.50, 0.25], 
+            'Unit': ['ml', 'g', 'Tbsps', 'g', 'large', 'Tbsps', 'Tbsps', 'tsps']}
+    df = pd.DataFrame(data)
+    df2 = test.getIngredients(1697687)
     assert df.equals(df2)
